5da6d5e7af899db9a6f43155d337181337daeaaa
waghchaureomkar <waghchaureomkar@gmail.com>
2024-05-12 20:04:15 +0530

    added cron jobs

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // import nodeVersion from "./jobs/nodeVersion.js";
 import cron from "node-cron";
 import nodeVersion from "./jobs/nodeVersion.js";
+import { cronTime1 } from "./utils/constants.js";
 
-cron.schedule('*/5 * * * * *', () => {
+cron.schedule(cronTime1, () => {
     console.log('Running a cron job at:', new Date());
     // nodeVersion();
     nodeVersion();
--- utils/constants.js
+++ utils/constants.js
@@ -1,1 +1,14 @@
 export const urlDB = "mongodb://localhost:27017/versions"
+
+// cron shedulars----------------------------------------------------------------
+
+export const cronTime1 = "*/5 * * * * *";   // every 5 seconds
+export const cronTime2 = "* * * * * *";     // every 1 minute
+export const cronTime3 = "0 * * * * *";     // every 1 hour
+export const cronTime4 = "0 */12 * * * *";  // twice day 
+export const cronTime5 = "0 0 0 * * *";     // once day
+export const cronTime6 = "0 0 * * 0 *";     // once week
+
+
+
+
